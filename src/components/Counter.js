291f377dbfe9ca37a1c714f6704226358d799b76
Aleksandr <it.kucherov@gmail.com>
2022-07-27 10:37:27 +0300

    connect

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,7 +1,11 @@
-const Counter = ({ conter, inc, dec, rnd }) => {
+import { connect } from 'react-redux/es/exports';
+import * as actions from '../actions';
+//import { bindActionCreators } from 'redux';
+
+const Counter = ({ counter, inc, dec, rnd }) => {
 	return (
 		<div className="jumbotron">
-			<h1>{conter}</h1>
+			<h1>{counter}</h1>
 			<button onClick={dec} className="btn btn-primary">
 				DEC
 			</button>
@@ -14,5 +18,18 @@ const Counter = ({ conter, inc, dec, rnd }) => {
 		</div>
 	);
 };
+//передача состояния
+const mapStateToProps = (state) => {
+	return {
+		counter: state.value,
+	};
+};
 
-export default Counter;
+//передача функции для изменения состояния
+//const mapDispatchToProps = (dispatch) => {
+//	return bindActionCreators(actions, dispatch);
+//};
+
+export default connect(mapStateToProps, actions)(Counter);
+
+//если переделать в классовый компонет, то все будет работать.
